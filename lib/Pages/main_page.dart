c6d9a31f2f8f6485bf1cd39573fdb2092cc3b377
DKzMarlen <Mark_damgaard@icloud.com>
2020-09-19 12:35:17 +0200

    mark :D xD

--- lib/Pages/main_page.dart
+++ lib/Pages/main_page.dart
@@ -10,6 +10,14 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+       title: Text("101 Drukspil"),
+        centerTitle: true,
+      ),
+      body: Text("OSRS er et legende spill",
+      textAlign: TextAlign.center,
+      style: TextStyle(fontSize: 30,color: Colors.red),),
+    );
   }
 }
