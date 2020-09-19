8ea994999227e74e77db27c90225ca6f608b443c
au619265 <au619265@uni.au.dk>
2020-09-19 12:44:43 +0200

    navbar

--- lib/Pages/main_page.dart
+++ lib/Pages/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:fancy_bottom_navigation/fancy_bottom_navigation.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -6,6 +7,13 @@ class MainPage extends StatefulWidget {
   @override
   _MainPageState createState() => _MainPageState();
 }
+
+int _currentIndex = 1;
+final List<Widget> _children = [
+  GamesLibraryWidget(),
+  FavoritePageWidget(),
+  SettingsWidget()
+];
 
 class _MainPageState extends State<MainPage> {
   @override
@@ -18,6 +26,20 @@ class _MainPageState extends State<MainPage> {
       body: Text("OSRS er et legende spill",
       textAlign: TextAlign.center,
       style: TextStyle(fontSize: 30,color: Colors.red),),
+      bottomNavigationBar: FancyBottomNavigation(
+          tabs:[
+            TabData(iconData:Icons.gamepad, title: "Mange Bajer"),
+            TabData(iconData: Icons.access_time, title: "Bajer"),
+            TabData(iconData: Icons.settings, title: "Indstillinger")
+          ],
+          onTabChangedListener: (int position) {
+            setState(() {
+              _currentIndex = position;
+            });
+          },
+          initialSelection: 1,
+          circleColor: Colors.orange,
+        )
     );
   }
 }
